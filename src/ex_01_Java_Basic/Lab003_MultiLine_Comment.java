e6bf086833e2b5b89959d2427eb09ccb2f452eed
info <9292rohit@gmail.com>
2026-02-01 17:21:48 +0530

    1st Feb.. code push5

--- src/ex_01_Java_Basic/Lab003_MultiLine_Comment.java
+++ src/ex_01_Java_Basic/Lab003_MultiLine_Comment.java
@@ -2,9 +2,10 @@ package ex_01_Java_Basic;
 
 public class Lab003_MultiLine_Comment {
     public static void main(String[] args) {
-        System.out.println("MultiLine_Comment");
-        //amend
-        //DSDS
-        //DASDAS
+        System.out.println("MultiLine_Comments");
+        /**amend
+        *DSDS
+         * DASDAS
+        **/
     }
 }
