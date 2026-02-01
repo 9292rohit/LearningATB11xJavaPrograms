7a530ea54ad0cfbe0dadd114ab450a30290b2bed
info <9292rohit@gmail.com>
2026-02-01 17:21:48 +0530

    1st Feb.. code push1

--- src/ex_01_Java_Basic/Lab002_Comments.java
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -3,10 +3,6 @@ package ex_01_Java_Basic;
 public class Lab002_Comments {
     public static void main(String[] args) {
         //comment single line
-        /** comment
-         * multi
-         * line
-         */
         System.out.println("Comments");
     }
 
