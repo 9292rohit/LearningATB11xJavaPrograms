7786337b59aed0b773be49fc6d572f66d9ede58b
info <9292rohit@gmail.com>
2026-03-02 19:41:07 +0530

    2nd march.. code push3

--- src/ex_01_Java_Basic/Lab004_Math_Operations.java
+++ src/ex_01_Java_Basic/Lab004_Math_Operations.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic;
 
-public class Math_Operations {
+public class Lab004_Math_Operations {
 
     public static void main(String[] args) {
         System.out.println(2+2);
---
+++ src/ex_01_Java_Basic/Lab005_variables.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab005_variables {
+
+        public static void main (String[] args)
+        {
+                System.out.println("Hellow");;
+        }
+
+}
