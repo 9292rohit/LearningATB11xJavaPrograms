35dff2d9f21dabc1ca50930e14f176ba0711ab3a
info <9292rohit@gmail.com>
2026-03-03 12:35:03 +0530

    3rd march.. code push

--- src/ex_01_Java_Basic/Lab005_variables.java
+++ src/ex_01_Java_Basic/Lab005_variables.java
@@ -2,9 +2,16 @@ package ex_01_Java_Basic;
 
 public class Lab005_variables {
 
-        public static void main (String[] args)
-        {
-                System.out.println("Hellow");;
-        }
+        public static void main (String[] args){
+                int a = 10;
+                a = a+1;
+//                a =10;
+                int b = 15;
+                int c = a * b;
+                int $ = a-b;
+                int Lab005_variables = a+b;
+                System.out.println($);
+                System.out.println(Lab005_variables);
 
+        }
 }
