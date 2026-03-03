f93c3aec9f4e7608ab59d270fee1be1b5fa03cc6
info <9292rohit@gmail.com>
2026-03-03 15:30:12 +0530

    3rd march.. code push

---
+++ src/ex_01_Java_Basic/Lab006_boolean.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab006_boolean {
+
+    public static void main (String[] args)
+    {
+        boolean Rohit_learning_Java = true;
+        boolean Rohit_have_Java_Knowledge = false;
+
+        System.out.println (Rohit_learning_Java);
+        System.out.println (Rohit_have_Java_Knowledge);
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab007_Numeric_Data_types.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Numeric_Data_types {
+
+    public static void main (String[] args){
+                byte a = -128;
+//                byte a = 128;
+                long b = 988117898;
+                long c = 9881178983L;
+
+
+
+                System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+
+
+
+
+    }
+}
