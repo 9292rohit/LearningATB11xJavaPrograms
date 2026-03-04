bf5898de46eff0be460a81ffc65ad0e319ce0d50
info <9292rohit@gmail.com>
2026-03-04 19:48:46 +0530

    4 march.. code push

---
+++ src/ex_01_Java_Basic/Lab011_constants.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab011_constants {
+
+    public static void main(String[] args) {
+        final int a = 10;
+//        a =11;  // not possible to change final value
+        System.out.println(a);
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab012_Local_variables.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab012_Local_variables {
+
+    public static void main (String[] args){
+
+        int a = 10;
+        byte b = 10;
+        short s = 10;
+        long l = 1111111L;
+        float f = 3.1416f;
+        double d = 23345678;
+        boolean  bl = true;
+        char c = 't';
+
+    }
+}
