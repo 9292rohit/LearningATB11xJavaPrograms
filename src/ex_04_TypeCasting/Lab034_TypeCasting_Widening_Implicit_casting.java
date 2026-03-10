3688c02ffcdbc3cd0ce54a9997c6bf014585b6de
info <9292rohit@gmail.com>
2026-03-10 11:59:25 +0530

    10 march.. code push

---
+++ src/ex_04_TypeCasting/Lab034_TypeCasting_Widening_Implicit_casting.java
@@ -0,0 +1,11 @@
+package ex_04_TypeCasting;
+
+public class Lab034_TypeCasting_Widening_Implicit_casting {
+    public static void main (String[] args){
+        byte a = 10;
+        int b = a;
+// Implicit casting_widening
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ src/ex_04_TypeCasting/Lab035_TypeCasting_Widening_Explicit_casting.java
@@ -0,0 +1,12 @@
+package ex_04_TypeCasting;
+
+public class Lab035_TypeCasting_Widening_Explicit_casting {
+
+    public static void main(String[] args) {
+        byte a = 10;
+        int b = (int)a;
+// Explicit casting - widening
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
